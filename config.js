9b81dd6d7760df557c4eb3ec7639295fe888377f
Македонски Медиа Сервис <76214265+MMS-mk@users.noreply.github.com>
2023-08-30 09:38:36 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 var settings = {
     'radio_name': 'Brasil Hits',
-    'url_streaming': 'https://radiocnd.mms.mk/proxy/eastgate/',
+    'url_streaming': 'https://streaming.brasilhits.com/acc',
     'streaming_type': 'icecast',
-    'api_key': '8bdadfdacfbf37333270410117cc0dcb',
+    'api_key': '18fe07917957c289983464588aabddfb',
     'historic': true,
     'next_song': false,
+
+    // Path to default cover art when none are found
     'default_cover_art': 'img/bg-capa.jpg',
 };
